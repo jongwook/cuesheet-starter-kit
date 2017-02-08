31a2638ea1a96a013a7b664ed9f51077ec336d13
Jong Wook Kim <jongwook@nyu.edu>
2017-02-08 14:42:43 -0500

    updated to SparkPi example

--- src/main/scala/CueSheetStarter.scala
+++ src/main/scala/CueSheetStarter.scala
@@ -1,5 +1,19 @@
 import com.kakao.cuesheet.CueSheet
+import org.apache.spark.rdd.RDD
 
-object CueSheetStarter extends CueSheet {
-  sc.parallelize(1 to 100).map(_ + 1).sum()
-}
+import scala.util.Random
+
+object CueSheetStarter extends CueSheet {{
+  val samples: RDD[Int] = sc.parallelize(1 to (1 << 24))
+  val numSamples = samples.count()
+
+  println(s"Using $numSamples samples to find the approximate value of pi")
+
+  val insideCircle = samples.map {
+    _ => (Random.nextDouble(), Random.nextDouble())
+  }.collect {
+    case (x, y) if x * x + y * y <= 1 => true
+  }.count()
+
+  println(s"Pi is roughly ${4.0 * insideCircle / numSamples}")
+}}
