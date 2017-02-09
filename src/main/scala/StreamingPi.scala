15c9d9b5be5c8d1fdbffe7ce9bc95baff8094852
Jong Wook Kim <jongwook@nyu.edu>
2017-02-09 09:01:07 -0500

    fixed StreamingPi

--- src/main/scala/StreamingPi.scala
+++ src/main/scala/StreamingPi.scala
@@ -12,12 +12,19 @@ object StreamingPi extends CueSheet {{
   }
 
   val inside = sc.longAccumulator("number of samples inside circle")
+  val total = sc.longAccumulator("total number of samples")
 
   samples.foreachRDD { rdd =>
-    val count = rdd.filter {
-      case (x, y) => x * x + y * y < 1
-    }.count()
-    println(s"Estimate of Pi with ${inside.count / M} million samples = ${4 * inside.avg}")
+
+    rdd
+      // accumulate the total number of samples
+      .mapPartitions { p => total.add(p.size); p }
+      // filter points inside the unit circle
+      .filter { case (x, y) => x*x + y*y < 1 }
+      // accumulate the number of them
+      .foreachPartition { p => inside.add(p.size) }
+
+    println(s"Estimate of Pi with ${total.sum / M} million samples = ${4.0f * inside.sum / total.sum}")
   }
 
 }}
